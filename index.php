f03651d169bd0082b0673360d88391bcaf0406ea
Andrea Caramma <andrea.caramma@soprasteria.com>
2019-10-11 11:12:06 +0200

    stampa ultima riga della query

--- index.php
+++ index.php
@@ -1,28 +1,16 @@
-<!DOCTYPE html>
 <html>
 <head>
 	<title>Wedding Planner</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 <div id= "container">
-	<div class="menu">
-	<form method="get" action="menu.php">
-		<div class="primo">
-			<span>
-				
-			</span>
-		</div>
-		<div class="secondo">
-			<span>
-				
-			</span>
-		</div>
-		<div class="dessert">
-			<span>
-				
-			</span>
-		</div>
+
+	<form method="post" action="menu.php">
+		<span>Seleziona il menù</span><br>
+		<input type="radio" name="menu" value="terra">Terra<br>
+		<input type="radio" name="menu" value="mare">Mare<br>
+		<input type="radio" name="menu" value="linea">Linea<br>
+		<span>Clicca per visualizzare i menù</span>
 		<input type="submit" name="Invio">
 	</form>
 	</div>
--- menu.php
+++ menu.php
@@ -3,18 +3,85 @@
 	$username = "root";
 	$password = "Ciao123@";
 	$dbname = "wedding";
+	
+	function console_log( $data ){
+	  echo '<script>';
+	  echo 'console.log('. json_encode( $data ) .')';
+	  echo '</script>';
+	}	
+
 	// connessione db
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn->connect_error) {
 	    die("Connessione fallita: " . $conn->connect_error);
 	}
 
+	$menu = $_POST["menu"];
 	//query
-	$primo = "SELECT nome FROM primo WHERE id=1";
-	$ris_primo = $conn->query($primo);
-	$secondo = "SELECT nome FROM secondo WHERE id=1";
-	$ris_secondo = $conn->query($secondo);
-	$dessert = "SELECT nome FROM dessert WHERE id=1";
-	$ris_dessert = $conn->query($dessert);
+	$query = "SELECT menu.nome_menu, primo.nome, secondo.nome, dessert.nome FROM primo INNER JOIN menu ON primo.id = menu.id_primo INNER JOIN secondo ON secondo.id = menu.id_secondo INNER JOIN dessert ON dessert.id = menu.id_dessert WHERE menu.nome_menu = '$menu'";
+
+	/*//query
+	$q_primo = "SELECT nome FROM primo WHERE id=1";
+	$ris_primo = $conn->query($q_primo);
+	$array_primo = $ris_primo->fetch_array(MYSQLI_ASSOC);	
+	foreach ($array_primo as $primo)
+
+	$q_secondo = "SELECT nome FROM secondo WHERE id=1";
+	$ris_secondo = $conn->query($q_secondo);
+	$array_secondo = $ris_secondo->fetch_array(MYSQLI_ASSOC);	
+	foreach ($array_secondo as $secondo)
+	  
+	$q_dessert = "SELECT nome FROM dessert WHERE id=1";
+	$ris_dessert = $conn->query($q_dessert);
+	$array_dessert = $ris_dessert->fetch_array(MYSQLI_ASSOC);	
+	foreach ($array_dessert as $dessert)
+
+
+	/*$crea_tab_primo = "CREATE TABLE primo (
+	id INT(10) PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(30) NOT NULL)";
+
+    $crea_tab_secondo = "CREATE TABLE secondo (
+	id INT(10) PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(30) NOT NULL)";
+
+    $crea_tab_dessert = "CREATE TABLE dessert (
+	id INT(10) PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(30) NOT NULL)";
+
+    $crea_tab_menu = "CREATE TABLE menu (
+	id INT(10) PRIMARY KEY AUTO_INCREMENT,
+	primo VARCHAR(30) FOREIGN KEY REFERENCES primo.id,
+	secondo VARCHAR(30) FOREIGN KEY REFERENCES secondo.id,
+	dessert VARCHAR(30) FOREIGN KEY REFERENCES dessert.id)";
+
+	$val_tab_primo = "INSERT INTO primo (id, nome)
+	VALUES (1, Pasta al pomodoro);"
+	$conn->query($val_tab_primo);
+
+	$val_tab_primo = "INSERT INTO primo (id, nome)
+	VALUES (2, Spaghetti alla carbonara);"
+	$conn->query($val_tab_primo);
+
+	$val_tab_primo = "INSERT INTO primo (id, nome)
+	VALUES (3, Spaghetti allo scoglio);"
+	$conn->query($val_tab_primo);*/
+
+	
+	
+	$result = $conn->query($query);
+	$array_menu = $result->fetch_array(MYSQLI_ASSOC);
+	foreach ($array_menu as $menus)
+		console_log($menus);
 	
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div class="menu">
+		<?php echo $menus ?>
+</body>
+</html>
